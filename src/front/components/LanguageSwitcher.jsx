b804399832c232edf9ec799263ab1c404ec3a02f
santgeek <santiago.mordz@gmail.com>
2026-05-14 15:26:25 -0500

    fix(ui): refactor LanguageSwitcher to use React state and fix styling

--- src/front/components/LanguageSwitcher.jsx
+++ src/front/components/LanguageSwitcher.jsx
@@ -6,6 +6,7 @@ import flagEN from '../assets/img/Flags/us.svg'
 function LanguageSwitcher() {
     const { i18n } = useTranslation();
     const [activeFlag, setActiveFlag] = useState(i18n.language === 'es' ? flagES : flagEN);
+    const [isOpen, setIsOpen] = useState(false);
 
     useEffect(() => {
         setActiveFlag(i18n.language === 'es' ? flagES : flagEN);
@@ -13,14 +14,32 @@ function LanguageSwitcher() {
 
     const changeLanguage = (lng) => {
         i18n.changeLanguage(lng);
+        setIsOpen(false);
     };
 
+    const toggleMenu = () => setIsOpen(prev => !prev);
+
     return (
-        <div className='btn-group'>
-            <button type="button" className="btn btn-link dropdown-toggle p-0" data-bs-toggle="dropdown" aria-expanded="false">
+        <div className={`btn-group ${isOpen ? 'show' : ''}`}>
+            <button
+                type="button"
+                className="btn btn-link dropdown-toggle p-0"
+                aria-expanded={isOpen}
+                onClick={toggleMenu}
+            >
                 <img src={activeFlag} alt="current language" className='flag-icon-main' />
             </button>
-            <ul className='dropdown-menu dropdown-menu-dark'>
+            <ul className={`dropdown-menu dropdown-menu-dark ${isOpen ? 'show' : ''}`}
+                style={{
+                    position: 'absolute',
+                    inset: '0px auto auto 0px',
+                    margin: '0px',
+                    transform: isOpen ? 'translate(-10px, 40px)' : 'none',
+                    display: isOpen ? 'block' : 'none',
+                    minWidth: '0',
+                    width: 'auto'
+                }}
+            >
                 <li>
                     <button className='dropdown-item' onClick={() => changeLanguage('es')}>
                         <img src={flagES} alt="Español" className='flag-icon-dropdown' />
