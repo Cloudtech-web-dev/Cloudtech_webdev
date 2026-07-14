f9d1444bd8b1a17f6bbfd626bba1ea4b7702b5c4
santgeek <santiago.mordz@gmail.com>
2026-07-14 18:52:29 -0500

    feat(contact): add Afterwards component for post-contact steps

--- src/front/pages/About.jsx
+++ src/front/pages/About.jsx
@@ -4,6 +4,7 @@ import { Values } from "../components/About/Values"
 import { WorkSteps } from "../components/About/WorkSteps"
 import { WhoWeAre } from "../components/About/WhoWeAre"
 import { SEO } from "../utils/SEO"
+import { Afterwards } from "../components/Contact/Afterwards"
 
 export const About = () => {
     return (
@@ -15,6 +16,7 @@ export const About = () => {
             />
             <main>
                 <HeaderAbout />
+                <Afterwards />
                 <WhoWeAre />
                 <Team />
                 <WorkSteps />
