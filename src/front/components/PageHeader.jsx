c2ac4d001d41ceae000c8b5ad5b63eab0ac54d89
mplac555 <marcelo.placencia@epn.edu.ec>
2026-07-28 21:45:39 -0500

    refactor(headers): reorganize `PageHeader` to handle transparency through props
    ---
    Also reorganized `Home` to have a cleaner source code. Changed the name of the transparent heading class from `.is-header` to `.synced-bg-heading` to be more specific, as this class is defined in the global stylesheet `index.css`.

--- src/front/components/PageHeader.jsx
+++ src/front/components/PageHeader.jsx
@@ -1,71 +1,94 @@
 import { Link } from "react-router-dom";
 import { useTranslation, Trans } from "react-i18next";
-import { useRef, useEffect } from "react";
+
 import { GradientBg } from "./GradientBg";
 
-const transparentText = true;
-const syncedBgClassName = "synced-bg";
-const syncedBg = (isHeading = true) => transparentText ? ` ${syncedBgClassName}${isHeading ? " is-header" : ""}` : "";
-const syncedBgStyles = (BgImport) => <style>{`
-    .${syncedBgClassName} {
-        background-image: url(${BgImport});
-        background-size: cover;
-        background-position: center;
-        background-attachment: fixed;
-    }
-    .is-header {                                            
-        color: transparent;
-        background-clip: text;
-    }
-`}</style>;
 
-export const PageHeader = ({ title, description, backgroundImg, withProjectButton = true, withSphereEffect = false, withoutBackgroundFilter = false }) => {
+export const PageHeader = ({
+  title,
+  description,
+  backgroundImg,
+  withProjectButton = true,
+  withSphereEffect = false,
+  withoutTransparencyEffect = false,
+  withoutBackgroundFilter = false
+}) => {
   const { t } = useTranslation();
-
+  
   return (
-    <section className={`position-relative min-vh-100 overflow-hidden${syncedBg(false)}`}>
+    <section className={"position-relative min-vh-100 overflow-hidden" + (withoutTransparencyEffect ? "" : " synced-bg")}>
 
+      {/* Background Selection */}
       {withSphereEffect ? (
         <div className="position-absolute top-0 start-0 w-100 h-100 z-0 overflow-hidden">
           <GradientBg />
         </div>
-      ) : (
-        !transparentText && (
-          <img
-            src={backgroundImg}
-            alt="CloudTech background image"
-            className="position-absolute top-0 start-0 w-100 h-100 object-fit-cover z-0"
-            style={{ objectPosition: "center, center" }}
-          />)
+      ) : withoutTransparencyEffect && (
+        <img  // ← Fallback if no transparency nor sphere effects
+          src={backgroundImg}
+          alt="CloudTech background image"
+          className="position-absolute top-0 start-0 w-100 h-100 object-fit-cover z-0"
+          style={{ objectPosition: "center, center" }} />
       )}
 
+      {/* (optional) Background Filter */}
       {withoutBackgroundFilter || <div className="position-absolute top-0 start-0 w-100 h-100 mx-auto" style={{ backgroundColor: "rgba(0, 0, 0, .55)", zIndex: 1 }} />}
 
+      {/* Section Contents */}
       <div className="container w-100 min-vh-100 py-5 d-flex align-items-center position-relative z-2">
         <div className="row w-100 z-0 d-flex justify-content-center">
           <div className="col-12 col-lg-8 d-flex flex-column justify-content-center align-items-center z-1 gap-5 w-auto">
+
+            {/* Heading */}
             <h1 className="hero-title-home display-2 fw-bolder text-center">
               <Trans
                 i18nKey={title}
-                components={[
-                  <span className={`highlighted-text ${syncedBg()}`} />
-                ]}
-              />
-              {transparentText && syncedBgStyles(backgroundImg)}
+                components={[ <span className={"highlighted-text" + (withoutTransparencyEffect ? "" : " synced-bg synced-bg-heading")} /> ]} />
             </h1>
 
+            {/* Description */}
             <p className="hero-subtitle-home fs-5 fw-bold text-white text-center w-75">
               {t(description)}
             </p>
 
+            {/* Navigation Buttons */}
             <div className="d-flex flex-column justify-content-center flex-md-row gap-3 w-100">
-              {withProjectButton && <Link to="/projects" className="btn btn-outline-light btn-lg rounded-pill px-5">{t('headers.headerAbout.portfolioButton')}</Link>}
-              <Link to="/contact" className="btn btn-outline btn-lg rounded-pill px-5">{t('headers.headerAbout.contactButton')}</Link>
+
+              {/* (optional) Projects Button */}
+              {withProjectButton && (
+                <Link to="/projects" className="btn btn-outline-light btn-lg rounded-pill px-5">
+                  {t('headers.headerAbout.portfolioButton')}
+                </Link>
+              )}
+
+              {/* Contact Button */}
+              <Link to="/contact" className="btn btn-outline btn-lg rounded-pill px-5">
+                {t('headers.headerAbout.contactButton')}
+              </Link>
+
             </div>
+
           </div>
         </div>
       </div>
+
+      {/* Component Styles */}
+      <style>{`
+        .synced-bg {
+            background-image: url(${backgroundImg});
+            background-size: cover;
+            background-position: center;
+            background-attachment: fixed;
+            
+            &.synced-bg-heading {
+                color: transparent;
+                background-clip: text;
+            }
+        }
+      `}</style>
+
     </section>
-  )
-}
+  );
+};
+
 export default PageHeader;
--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -1,34 +1,28 @@
-import { Services } from "../components/Services/Services.jsx";
-import { Process } from "../components/Process.jsx";
-import { Testimonials } from "../components/Testimonial/Testimonials.jsx";
-import { PageHeader } from  "../components/PageHeader";
-import { Introduction } from "../components/Introduction.jsx";
-import { Benefits } from "../components/Benefits/Benefits.jsx";
-import { WorkWithUS } from "../components/WorkWithUs.jsx";
-import { ProjectsHome } from "../components/ProjectsHome.jsx";
 import { SEO } from "../utils/SEO.jsx";
 
-export const Home = () => {
-	return (
-		<>
-			<SEO
-				title="Desarrollo Web y Comunicación Estratégica"
-				description="Transformamos tu presencia digital con diseño web de alto impacto y comunicación estratégica. Conoce nuestro portafolio y por qué nuestros clientes nos recomiendan."
-				url={window.location.href}
-			/>
-			<main>
-				<PageHeader
-					title={'headers.headerHome.headLine'}
-					description={'headers.headerHome.headerDescription'}
-					backgroundImg={"src/front/assets/img/HomeOne.jpg"}
-					withoutBackgroundFilter
-				/>
-				<Introduction />
-				<Services />
-				<ProjectsHome />
-				<Testimonials />
-				<WorkWithUS />
-			</main>
-		</>
-	);
-};
+import { PageHeader } from "../components/PageHeader";
+import { Introduction } from "../components/Introduction.jsx";
+import { Services } from "../components/Services/Services.jsx";
+import { ProjectsHome } from "../components/ProjectsHome.jsx";
+import { Testimonials } from "../components/Testimonial/Testimonials.jsx";
+import { WorkWithUS } from "../components/WorkWithUs.jsx";
+
+
+export const Home = () => (<>
+	<SEO
+		title="Desarrollo Web y Comunicación Estratégica"
+		description="Transformamos tu presencia digital con diseño web de alto impacto y comunicación estratégica. Conoce nuestro portafolio y por qué nuestros clientes nos recomiendan."
+		url={window.location.href} />
+	<main>
+		<PageHeader
+			title={'headers.headerHome.headLine'}
+			description={'headers.headerHome.headerDescription'}
+			backgroundImg={"src/front/assets/img/HomeOne.jpg"}
+			withoutBackgroundFilter />
+		<Introduction />
+		<Services />
+		<ProjectsHome />
+		<Testimonials />
+		<WorkWithUS />
+	</main>
+</>);
