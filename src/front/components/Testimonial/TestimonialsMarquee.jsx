25e8fc222c3f901288465ce912065e9a8d0dd3b2
mplac555 <marcelo.placencia@epn.edu.ec>
2026-08-20 20:41:47 -0500

    fix(testimonials-marquee): remove most bugs from interactions with marquee
    ---
    There is still some imperfections on the feedback, and small inconsistencies, but overall decent results.

--- src/front/components/Testimonial/TestimonialsMarquee.jsx
+++ src/front/components/Testimonial/TestimonialsMarquee.jsx
@@ -7,6 +7,7 @@ import styles from "../../styles/components/HomeTestimonials.module.css";
 
 
 const scrollProgressCSSVar = '--marquee-scroll-progress-percentage';
+const swipeMinDelta = 10;
 
 
 /**
@@ -22,8 +23,9 @@ const scrollProgressCSSVar = '--marquee-scroll-progress-percentage';
 export const TestimonialsMarquee = ({ cardsArray = [], className: propClassNames, rollDuration = '30s' }) => {
   const { t } = useTranslation();
 
+  /* MARQUEE GROUP CLONES */
+  
   const [duplicatesCount, setDuplicatesCount] = useState(2);
-
 
   useEffect(() => {
 
@@ -46,27 +48,34 @@ export const TestimonialsMarquee = ({ cardsArray = [], className: propClassNames
   }, []);
   
 
-  /** @type {React.MutableRefObject<HTMLDivElement?>} */
+  /* MARQUEE INTERACTION HANDLING */
+
+  /**
+   * @type {React.MutableRefObject<HTMLDivElement?>}
+   * */
   const wrapperRef = useRef(null);
-  /** @type {React.MutableRefObject<HTMLDivElement?>} */
+  /**
+   * @type {React.MutableRefObject<HTMLDivElement?>}
+   * */
   const trackRef = useRef(null);
 
   const isDragging = useRef(false);
+  const isGrabbing = useRef(false);
   const startX = useRef(0);
   const currentDragOffset = useRef(0);
   const currentProgress = useRef(0);
-  /** @type {React.MutableRefObject<NodeJS.Timeout|undefined>} */
-  const wheelTimeout = useRef();
 
-  const startDrag = () => {
+  const startDrag = (options={ grab: true }) => {
     if (!wrapperRef.current || !trackRef.current) return;
 
+    isGrabbing.current = !(options?.grab==false);
     isDragging.current = true;
     trackRef.current.classList.add(styles['is-dragging']);
   };
   const stopDrag = () => {
     if (!wrapperRef.current || !trackRef.current) return;
 
+    isGrabbing.current = false;
     isDragging.current = false;
     startX.current = 0;
     currentDragOffset.current = 0;
@@ -74,7 +83,6 @@ export const TestimonialsMarquee = ({ cardsArray = [], className: propClassNames
 
     trackRef.current.classList.remove(styles['is-dragging']);
   };
-
   const getCurrentGroupWidth = () => (
     trackRef.current?.getElementsByClassName(styles['marquee-group'])[0]?.clientWidth ?? 1
   );
@@ -87,15 +95,20 @@ export const TestimonialsMarquee = ({ cardsArray = [], className: propClassNames
     trackRef.current?.style.setProperty(scrollProgressCSSVar, String(progress))
   };
 
+  /* Pointer (e.g. mouse cursor) Interaction */
+  
   const handlePointerDown = (/**@type{React.PointerEvent<HTMLDivElement>}*/e) => {
     if (!wrapperRef.current || !trackRef.current) return;
 
     startDrag();
+    isGrabbing.current = true;
 
     startX.current = e.clientX;
     getCurrentProgress();
 
     e.currentTarget.setPointerCapture(e.pointerId);
+
+    document.addEventListener('pointermove', handlePointerMove, {passive: true});
   };
   const handlePointerMove = (/**@type{React.PointerEvent<HTMLDivElement>}*/e) => {
     if (!wrapperRef.current || !trackRef.current || !isDragging.current) return;
@@ -112,24 +125,71 @@ export const TestimonialsMarquee = ({ cardsArray = [], className: propClassNames
   const handlePointerEnd = (/**@type{React.PointerEvent<HTMLDivElement>}*/e) => {
     if (!wrapperRef.current || !trackRef.current) return;
 
+    isGrabbing.current = false;
     stopDrag();
     e.currentTarget.releasePointerCapture(e.pointerId);
+    document.removeEventListener('pointermove', handlePointerMove);
   };
+
+  /* Touch Interaction */
+  
+  /**
+   * @type {React.MutableRefObject<Touch>}
+   * */
+  const touchStart = useRef();
+  /**
+   * @type {React.MutableRefObject<boolean?>}
+   * */
+  const isInitialTouch = useRef(null);
+  const isHorizontallyLocked = useRef(false);
 
   const handleTouchStart = (/**@type{React.TouchEvent<HTMLDivElement>}*/e) => {
     if (!wrapperRef.current || !trackRef.current) return;
 
-    e.preventDefault();
+    touchStart.current = e.touches[0];
+    isInitialTouch.current = true;
 
-    startDrag();
+    // startDrag();
+    wrapperRef.current.addEventListener('touchmove', handleTouchMove, { passive: false });
   };
+  const handleTouchMove = (/**@type{React.TouchEvent<HTMLDivElement>}*/e) => {
+    if (!wrapperRef.current || !trackRef.current) return;
+    
+    if (isInitialTouch.current) {
+      isInitialTouch.current = false;
+      
+      const touchEnd = e.touches[0];
+      const deltaX = Math.abs(touchEnd.clientX - touchStart.current.clientX);
+      const deltaY = Math.abs(touchEnd.clientY - touchStart.current.clientY);
+      
+      if (deltaX > Math.max(swipeMinDelta, deltaY)) {
+        // isGrabbing.current = true;
+        startDrag();
+        isHorizontallyLocked.current = true;
+      }
+    }
+    
+    isHorizontallyLocked.current && e.cancelable && e.preventDefault();
+  }
   const handleTouchEnd = (/**@type{React.TouchEvent<HTMLDivElement>}*/e) => {
     if (!wrapperRef.current || !trackRef.current) return;
+    
+    isHorizontallyLocked.current = false;
+    isInitialTouch.current = null;
 
+    // isGrabbing.current = false;
     stopDrag();
+    wrapperRef.current.removeEventListener('touchmove', handleTouchMove);
   };
 
-  const handleHorizontalScroll = (/**@type{WheelEvent}*/e) => {
+  /* Scroll Interaction */
+
+  /**
+   * @type {React.MutableRefObject<NodeJS.Timeout|undefined>}
+   * */
+  const wheelTimeout = useRef();
+
+  const handleHorizontalScroll = (/**@type{React.WheelEvent<HTMLDivElement>}*/e) => {
     if (!wrapperRef.current || !trackRef.current) return;
 
     let deltaX, deltaY;
@@ -144,7 +204,9 @@ export const TestimonialsMarquee = ({ cardsArray = [], className: propClassNames
     
     e.preventDefault();
 
-    startDrag();
+    if (isGrabbing.current) return clearTimeout(wheelTimeout.current);
+    
+    startDrag({ grab: false });
 
     const
       adjustedScrollDelta = deltaX * (e.shiftKey ? 0.2 : 0.8),
@@ -160,28 +222,31 @@ export const TestimonialsMarquee = ({ cardsArray = [], className: propClassNames
     wheelTimeout.current = setTimeout(stopDrag, 250);
   };
 
+  /* Non-passive Event Listeners Mounting */
+
   useEffect(() => {
     if (!wrapperRef.current || !trackRef.current) return;
 
+    console.log("hohohoooo");
+    
+    wrapperRef.current.addEventListener('pointerdown', handlePointerDown, { passive: false });
+    wrapperRef.current.addEventListener('touchstart', handleTouchStart, { passive: false });
     wrapperRef.current.addEventListener('wheel', handleHorizontalScroll, { passive: false });
-    return () => wrapperRef.current?.removeEventListener('wheel', handleHorizontalScroll);
-
+    
+    return () => {
+      console.log("cloooooose");
+      wrapperRef.current.removeEventListener('pointerdown', handlePointerDown);
+      wrapperRef.current.removeEventListener('touchstart', handleTouchStart);
+      wrapperRef.current?.removeEventListener('wheel', handleHorizontalScroll);
+    };
   }, [wrapperRef.current]);
   
 
   return (
     <div className={styles["marquee-wrapper"] + (propClassNames || "")}
-
       ref={wrapperRef}
-
-      onPointerDown={handlePointerDown}
-      onPointerMove={handlePointerMove}
-      onPointerUp={handlePointerEnd}
-      onPointerCancel={handlePointerEnd}
-
-      onTouchStart={handleTouchStart}
-      onTouchEnd={handleTouchEnd}
-      onTouchCancel={handleTouchEnd}
+      onPointerUp={handlePointerEnd} onPointerCancel={handlePointerEnd}
+      onTouchEnd={handleTouchEnd} onTouchCancel={handleTouchEnd}
     >
       <div className={styles["marquee-track"]} ref={trackRef} style={{
         '--marquee-translate-delta': duplicatesCount,
